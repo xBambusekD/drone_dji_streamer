4f8b52321e2b09ad032fe1c211e7912eb9026510
xBambusekD <bambusek7619@gmail.com>
2021-10-27 16:23:51 +0200

    DJI streamer app

---
+++ DjiStreamer/app/src/main/java/com/dji/dronedjistreamer/DemoApplication.java
@@ -0,0 +1,235 @@
+package com.dji.dronedjistreamer;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+
+import dji.common.error.DJIError;
+import dji.common.error.DJISDKError;
+import dji.common.flightcontroller.LocationCoordinate3D;
+import dji.common.useraccount.UserAccountState;
+import dji.common.util.CommonCallbacks;
+import dji.sdk.base.BaseComponent;
+import dji.sdk.base.BaseProduct;
+import dji.sdk.flightcontroller.FlightController;
+import dji.sdk.products.Aircraft;
+import dji.sdk.sdkmanager.DJISDKInitEvent;
+import dji.sdk.sdkmanager.DJISDKManager;
+import dji.sdk.useraccount.UserAccountManager;
+
+public class DemoApplication extends Application {
+
+    public static final String FLAG_CONNECTION_CHANGE = "uxsdk_demo_connection_change";
+
+    private static BaseProduct mProduct;
+    private Handler mHandler;
+    private DJISDKManager.SDKManagerCallback mDJISDKManagerCallback;
+
+    private static Application instance;
+    public void setContext(Application application) {
+        instance = application;
+    }
+
+    public static Application getInstance() {
+        return DemoApplication.instance;
+    }
+
+    @Override
+    public Context getApplicationContext() {
+        return instance;
+    }
+
+    public DemoApplication() {
+
+    }
+
+    /**
+     * This function is used to get the instance of DJIBaseProduct.
+     * If no product is connected, it returns null.
+     */
+    public static synchronized BaseProduct getProductInstance() {
+        if (null == mProduct) {
+            mProduct = DJISDKManager.getInstance().getProduct();
+        }
+        return mProduct;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mHandler = new Handler(Looper.getMainLooper());
+
+        //Check the permissions before registering the application for android system 6.0 above.
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permissionCheck2 = ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.READ_PHONE_STATE);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || (permissionCheck == 0 && permissionCheck2 == 0)) {
+
+            //This is used to start SDK services and initiate SDK.
+            DJISDKManager.getInstance().registerApp(getApplicationContext(), mDJISDKManagerCallback);
+        } else {
+            Toast.makeText(getApplicationContext(), "Please check if the permission is granted.", Toast.LENGTH_LONG).show();
+        }
+
+        /**
+         * When starting SDK services, an instance of interface DJISDKManager.DJISDKManagerCallback will be used to listen to
+         * the SDK Registration result and the product changing.
+         */
+        mDJISDKManagerCallback = new DJISDKManager.SDKManagerCallback() {
+
+            //Listens to the SDK registration result
+            @Override
+            public void onRegister(DJIError error) {
+                if(error == DJISDKError.REGISTRATION_SUCCESS) {
+                    DJISDKManager.getInstance().startConnectionToProduct();
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "Register Success", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    loginAccount();
+
+                } else {
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "Register Failed, check network is available", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+                Log.e("TAG", error.toString());
+                updateAircraftFlightData("DJI onRegister");
+            }
+
+            @Override
+            public void onProductDisconnect() {
+                Log.d("TAG", "onProductDisconnect");
+                Toast.makeText(getApplicationContext(), "onProductDisconnect", Toast.LENGTH_LONG).show();
+                notifyStatusChange();
+                updateAircraftFlightData("DJI onProductDisconnected");
+            }
+            @Override
+            public void onProductConnect(BaseProduct baseProduct) {
+                Log.d("TAG", String.format("onProductConnect newProduct:%s", baseProduct));
+                Toast.makeText(getApplicationContext(), "onProductConnect", Toast.LENGTH_LONG).show();
+                notifyStatusChange();
+                updateAircraftFlightData("DJI onProductConnected");
+            }
+
+            @Override
+            public void onProductChanged(BaseProduct baseProduct) {
+                Log.d("TAG", String.format("onProductChanged newProduct:%s", baseProduct));
+                Toast.makeText(getApplicationContext(), "onProductChanged", Toast.LENGTH_LONG).show();
+                notifyStatusChange();
+                updateAircraftFlightData("DJI onProductChanged");
+            }
+            @Override
+            public void onComponentChange(BaseProduct.ComponentKey componentKey, BaseComponent oldComponent,
+                                          BaseComponent newComponent) {
+                if (newComponent != null) {
+                    newComponent.setComponentListener(new BaseComponent.ComponentListener() {
+
+                        @Override
+                        public void onConnectivityChange(boolean isConnected) {
+                            Log.d("TAG", "onComponentConnectivityChanged: " + isConnected);
+                            notifyStatusChange();
+                        }
+                    });
+                }
+
+                Log.d("TAG",
+                        String.format("onComponentChange key:%s, oldComponent:%s, newComponent:%s",
+                                componentKey,
+                                oldComponent,
+                                newComponent));
+
+            }
+            @Override
+            public void onInitProcess(DJISDKInitEvent djisdkInitEvent, int i) {
+
+            }
+
+            @Override
+            public void onDatabaseDownloadProgress(long l, long l1) {
+
+            }
+
+        };
+
+    }
+
+    private void updateAircraftFlightData(String msg) {
+//                Aircraft aircraft = (Aircraft) baseProduct;
+//                FlightController flightController = aircraft.getFlightController();
+
+        if (MainActivity.webSocketClient == null) {
+            MainActivity.createWebSocketClient();
+            MainActivity.webSocketClient.connect();
+        }
+
+        Handler handler = new Handler();
+        int delay = 1000;
+        //DJISDKManager.getInstance().getFlightHubManager().getAircraftRealTimeFlightData();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //LocationCoordinate3D location = flightController.getState().getAircraftLocation();
+                //MainActivity.webSocketClient.send("DJI: altitude: " + location.getAltitude() + " | latitude: " + location.getLatitude() + " | longitude: " + location.getLongitude());
+                MainActivity.webSocketClient.send(msg);
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+    }
+
+    private void loginAccount(){
+
+        UserAccountManager.getInstance().logIntoDJIUserAccount(this,
+                new CommonCallbacks.CompletionCallbackWith<UserAccountState>() {
+                    @Override
+                    public void onSuccess(final UserAccountState userAccountState) {
+                        Log.e("TAG", "Login Success");
+                    }
+                    @Override
+                    public void onFailure(DJIError error) {
+                        Log.e("TAG", "Login Error:" + error.getDescription());
+                    }
+                });
+    }
+
+    private void notifyStatusChange() {
+        mHandler.removeCallbacks(updateRunnable);
+        mHandler.postDelayed(updateRunnable, 500);
+    }
+
+    private Runnable updateRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            Intent intent = new Intent(FLAG_CONNECTION_CHANGE);
+            getApplicationContext().sendBroadcast(intent);
+        }
+    };
+
+    public static boolean isAircraftConnected() {
+        return getProductInstance() != null && getProductInstance() instanceof Aircraft;
+    }
+
+    public static synchronized Aircraft getAircraftInstance() {
+        if (!isAircraftConnected()) {
+            return null;
+        }
+        return (Aircraft) getProductInstance();
+    }
+
+}
